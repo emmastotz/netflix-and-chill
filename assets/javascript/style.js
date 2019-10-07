f1bda61eb28fec196dfd103d80e244aa861dd485
Emma Stotz <stotz.emma@gmail.com>
2019-10-06 21:26:39 -0400

    Fixed animation issues. Tweaked styling to make instructions clear for user.

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
+  $(".get-started").show();
   $("#food-input").hide();
   $("#movie-input").hide();
   $("#food-display").hide();
   $("#movie-display").hide();
+  $(".get-started").show();
 
   $(".netflix-logo").on("click", function(event){
+    $(".get-started").hide();
     $("#food-input").show();
     $("#movie-input").hide();
     $("#food-display").hide();
     $("#movie-display").hide();
 
     $("#food-submit-btn").on("click", function(event){
+      $(".get-started").hide();
       $("#food-input").hide();
       $("#movie-input").show();
       $("#food-display").hide();
@@ -18,6 +22,7 @@ $(document).ready(function(){
     });
     
     $("#movie-submit-btn").on("click", function(event){
+      $(".get-started").hide();
       $("#food-input").hide();
       $("#movie-input").hide();
       $("#food-display").show();
